1e5ad9bdca24ee64281418ff597f2c97f3074cdc
Auwate <auwate1@gmail.com>
2024-04-08 13:23:25 -0400

    backend changes

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .models import Address
 from .serializers import AddressSerializer
 
@@ -54,3 +55,20 @@ def getRecentQueries (request):
     queries = Address.objects.all()
     serializer = AddressSerializer(queries, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def postData(request):
+    if request.method == 'POST':
+
+        data = request.data # Extract
+        serializer = AddressSerializer(data=data) # Serialize into an object
+        
+        # Validate
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Success", status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        

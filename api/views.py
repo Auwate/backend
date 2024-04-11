24d771291028f943f5f2b7b34501d94c12bc7458
Auwate <auwate1@gmail.com>
2024-04-11 14:54:53 -0400

    - Commit:
    Fix ChatGPT API

--- api/views.py
+++ api/views.py
@@ -68,7 +68,7 @@ def getResponse (request):
             messages=[{"role": "user", "content": "Explain to me who" + addr.body + "is and what" + addr.body + "might contain. Do not tell me that you cannot browse, I already know."}],
         )
         # Serialize for sending through HTTP
-        serializer = OpenAISerializer(data={'response': response.choices[0].text})
+        serializer = OpenAISerializer(data={'response': response.choices[0].message.content})
         # Check if serializer was correct
         if serializer.is_valid():
             return Response(serializer.data)
